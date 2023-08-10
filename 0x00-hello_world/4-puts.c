58bc70370ac94b01385b726077afcc1803531566
reetech22 <reetech22@gmail.com>
2023-08-10 01:49:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  my main function
+ *
+ * Return: value is always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
