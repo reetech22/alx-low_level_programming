52a55a5025e210d3f28de70feb25ea4ea0e0b0d4
reetech22 <reetech22@gmail.com>
2023-11-03 10:43:27 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,37 +15,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+unsigned int i, j, k;
 char *concat;
-unsigned int s1_len = 0;
-unsigned int s2_len = 0;
-unsigned int j, k;
+
 
 if (s1 == NULL)
-s1 = "";
+i = 0;
+
+for (i = 0; s1[i]; ++i)
+;
 if (s2 == NULL)
-s2 = "";
+j = 0;
 
-while (s1[s1_len])
-s1_len++;
-while (s2[s2_len])
-s2_len++;
-
-if (n >= s2_len)
-n = s2_len;
-
-concat = malloc(s1_len + n + 1);
-
+for (j = 0; s2[j]; ++j)
+;
+if (j > n)
+j = n;
+concat = malloc(sizeof(char) * (i + j + 1));
 if (concat == NULL)
 return (NULL);
-
-for (j = 0; j < s1_len; j++)
-concat[j] = s1[j];
-
-for (k = 0; k < n; k++, k++)
-concat[k] = s2[k];
-
-concat[j] = '\0';
-
+for (k = 0; k < i; k++)
+concat[k] = s1[k];
+for (k = 0; k < j; k++)
+concat[k + i] = s2[k];
+concat[i + j] = '\0';
 return (concat);
 }
 
